5a78cc4b823e9914f2b9a3a1a005c4ec7f3d93ea
bartek-kachnic123 <bartek.kachnic123@gmail.com>
2025-03-31 15:18:46 +0200

    Add cors, session, and auth config

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan'); // middleware do kolorowych logów
+const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport')
 
 const usersRouter = require('./routes/users');
 const registerRouter = require('./routes/register');
@@ -8,11 +11,34 @@ const loginRouter = require('./routes/login');
 const postsRouter = require('./routes/posts');
 
 const app = express();
+const authLocalStrategy = require("./auth/authLocalStrategy");
 
+app.use(cors({
+    origin: process.env.REACT_ORIGIN || 'http://localhost:3001',
+    credentials: true
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+passport.use(authLocalStrategy.createStrategy());
+passport.serializeUser(authLocalStrategy.serializeUser());
+passport.deserializeUser(authLocalStrategy.deserializeUser());
+
+app.use(
+    session({
+        secret: process.env.SESSION_KEY || "SECRET_SESSION_KEY",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 60000 * 60,
+        }, // TODO: Add sqlite session store with connect-session-sequelize
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session({}));
 
 
 app.use('/users', usersRouter);
---
+++ server/auth/authLocalStrategy.js
@@ -0,0 +1,53 @@
+const LocalStrategy = require('passport-local').Strategy;
+const db = require('../models');
+const PasswordManager = require("./passwordManager");
+
+const createStrategy = () => {
+    return new LocalStrategy(
+        {
+            usernameField: 'email',
+            passwordField: 'password'
+        },
+        async (email, password, done) => {
+            try {
+                const user = await db.User.findOne({
+                    where: { email: email }
+                });
+
+                if (!user) {
+                    return done(null, false, { message: 'User does not exist' });
+                }
+
+                const isPasswordValid = PasswordManager.compare(password, user.password);
+                if (!isPasswordValid) {
+                    return done(null, false, { message: 'Bad Credentials' });
+                }
+
+                // If everything is good, return user
+                return done(null, user);
+            } catch (err) {
+                return done(err, null);
+            }
+        }
+    );
+}
+
+const serializeUser = () =>  {
+    return ((user, done) => {
+    done(null, user.id);
+    })
+};
+
+const deserializeUser = () => {
+    return (async (id, done) => {
+        try {
+            const user = await db.User.findByPk(id);
+            if (!user) return done(null, false, { message: 'User does not exist' });
+            done(null, user);
+        } catch (err) {
+            done(err, null);
+        }
+    });
+}
+
+module.exports = {createStrategy, deserializeUser, serializeUser};
