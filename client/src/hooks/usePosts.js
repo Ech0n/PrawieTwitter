cad98a3154edd9f7a684a6a935961168ac59d968
tomaszpakula <tomasz.pakula11@gmail.com>
2025-06-05 01:08:44 +0200

    get post by specific user

--- client/src/hooks/usePosts.js
+++ client/src/hooks/usePosts.js
@@ -17,5 +17,21 @@ export default function usePosts() {
     }
   };
 
-  return { getPosts };
+  const getPostByUser = async (id) => {
+    const response = await fetch(`http://localhost:3000/posts?owner_id=${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+
+    const data = await response.json()
+    if(!response.ok) throw new Error(data.error || "Could not load posts");
+    else{
+        return data.posts;
+    }
+  }
+
+  return { getPosts, getPostByUser };
 }
